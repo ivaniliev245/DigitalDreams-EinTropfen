9a592269770aab8ec88251b43d4d586f237d75dd
ivaniliev245 <131882120+ivaniliev245@users.noreply.github.com>
2024-11-13 16:48:14 +0100

    play manu
    play manu

---
+++ Assets/scripts/UIManager.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+using UnityEngine.UI;  // For UI components
+using UnityEngine.SceneManagement;  // For scene loading
+
+public class UIManager : MonoBehaviour
+{
+    public Button playButton;
+    public Button exitButton;
+    public Button muteButton;
+    public Slider volumeSlider;
+    public AudioSource audioSource;
+
+    private bool isMuted = false;
+
+    void Start()
+    {
+        // Set the initial volume based on the slider
+        volumeSlider.value = PlayerPrefs.GetFloat("Volume", 1f);
+        audioSource.volume = volumeSlider.value;
+
+        // Add listeners to buttons and slider
+        playButton.onClick.AddListener(PlayGame);
+        exitButton.onClick.AddListener(ExitGame);
+        muteButton.onClick.AddListener(MuteAudio);
+        volumeSlider.onValueChanged.AddListener(SetVolume);
+    }
+
+    void PlayGame()
+    {
+        // Load the scene to start the game (replace "GameScene" with your actual scene name)
+        SceneManager.LoadScene("Main_Game");
+    }
+
+    void ExitGame()
+    {
+        // Exit the application
+        Application.Quit();
+
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;  // Stop play mode in editor
+        #endif
+    }
+
+    void MuteAudio()
+    {
+        isMuted = !isMuted;
+        audioSource.mute = isMuted;
+        muteButton.GetComponentInChildren<Text>().text = isMuted ? "Unmute" : "Mute";  // Change button text
+    }
+
+    void SetVolume(float volume)
+    {
+        audioSource.volume = volume;
+        PlayerPrefs.SetFloat("Volume", volume);  // Save volume setting
+    }
+}
