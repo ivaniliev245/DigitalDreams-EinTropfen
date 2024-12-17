08632400e7c9185d35036f03bcbbd6f6b77442dd
ivaniliev245 <131882120+ivaniliev245@users.noreply.github.com>
2024-12-17 12:43:52 +0100

    jiggle physics

---
+++ Assets/JigglePhysics/Scripts/JiggleRigFixedUpdateHandler.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace JigglePhysics {
+
+internal class JiggleRigFixedUpdateHandler : JiggleRigHandler<JiggleRigFixedUpdateHandler> {
+    private void FixedUpdate() {
+        CachedSphereCollider.EnableSphereCollider();
+        var gravity = Physics.gravity;
+        var deltaTime = Time.deltaTime;
+        var timeAsDouble = Time.timeAsDouble;
+        var timeAsDoubleOneStepBack = timeAsDouble - JiggleRigBuilder.VERLET_TIME_STEP;
+        if (!CachedSphereCollider.TryGet(out SphereCollider sphereCollider)) {
+            throw new UnityException( "Failed to create a sphere collider, this should never happen! Is a scene not loaded but a jiggle rig is?");
+        }
+        foreach (var jiggleRig in jiggleRigs) {
+            jiggleRig.Advance(deltaTime, gravity, timeAsDouble, timeAsDoubleOneStepBack, sphereCollider);
+        }
+        CachedSphereCollider.DisableSphereCollider();
+        JiggleRigBuilder.unitySubsystemFixedUpdateRegistration = false;
+    }
+}
+
+}
---
+++ Assets/JigglePhysics/Scripts/JiggleRigLateUpdateHandler.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace JigglePhysics {
+
+internal class JiggleRigLateUpdateHandler : JiggleRigHandler<JiggleRigLateUpdateHandler> {
+    private void LateUpdate() {
+        CachedSphereCollider.EnableSphereCollider();
+        var gravity = Physics.gravity;
+        var deltaTime = Time.deltaTime;
+        var timeAsDouble = Time.timeAsDouble;
+        var timeAsDoubleOneStepBack = timeAsDouble - JiggleRigBuilder.VERLET_TIME_STEP;
+        if (!CachedSphereCollider.TryGet(out SphereCollider sphereCollider)) {
+            throw new UnityException( "Failed to create a sphere collider, this should never happen! Is a scene not loaded but a jiggle rig is?");
+        }
+        foreach (var jiggleRig in jiggleRigs) {
+            jiggleRig.Advance(deltaTime, gravity, timeAsDouble, timeAsDoubleOneStepBack, sphereCollider);
+        }
+        CachedSphereCollider.DisableSphereCollider();
+        JiggleRigBuilder.unitySubsystemLateUpdateRegistration = false;
+    }
+}
+
+}
